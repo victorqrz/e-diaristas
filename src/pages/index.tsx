498a6ce0c5399f7f58529ef00bf487b3298390e8
Victor Queiroz <victoorqrz@gmail.com>
2021-06-12 13:07:07 -0300

    creating SafeEnvironment, PageTitle and app theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment';
+import PageTitle from 'ui/components/data-display/PageTitle';
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <SafeEnvironment />
+      <PageTitle 
+        title={'Conheça os profissionais'}
+        subtitle={'Preencha seu endereço e veja todos os profissionais da sua cidade'}
+      />
+    </div>
   )
 }
