63369aae0ea20a988727a378008629aa6c6a9275
Victor Queiroz <victoorqrz@gmail.com>
2021-06-12 15:45:03 -0300

    UserInformation done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,38 @@
-import Head from 'next/head';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment';
-import PageTitle from 'ui/components/data-display/PageTitle';
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation";
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
-        title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais da sua cidade'}
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua cidade"
+        }
+      />
+
+      <UserInformation
+        name={"Victor Souza"}
+        picture={"https://avatars.githubusercontent.com/u/38059087?v=4"}
+        rating={4}
+        description={"Aracaju"}
+      />
+
+      <UserInformation
+        name={"Eduardo Costa"}
+        picture={"https://avatars.githubusercontent.com/u/38059087?v=4"}
+        rating={2}
+        description={"Minas Gerais"}
+      />
+
+      <UserInformation
+        name={"Axl Rose"}
+        picture={"https://avatars.githubusercontent.com/u/38059087?v=4"}
+        rating={5}
+        description={"Porto Velho"}
       />
     </div>
-  )
+  );
 }
