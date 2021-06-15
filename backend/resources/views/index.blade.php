6b5ce89564f786aba8c0cc8c2c5ffc23d6b95408
Victor Queiroz <victoorqrz@gmail.com>
2021-06-15 19:55:35 -0300

    adding bootstrap and listing diaristas

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -11,9 +11,28 @@
     <title>E-Diaristas</title>
   </head>
   <body>
-    <h1>Página Inicial E-Diaristas</h1>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('diaristas.index') }}">E-Diaristas</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('diaristas.index') }}">
+                            Lista de Diaristas
+                        </a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
     <div class="container">
+        <h1>Lista de Diaristas</h1>
         <table class="table">
             <thead>
                 <tr>
